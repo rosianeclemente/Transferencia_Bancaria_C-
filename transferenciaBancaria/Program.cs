8dab6f586c264c7a4d7564a1b5a1aeae36db2c58
Rosiane <rosianeclemente1997@gmail.com>
2021-10-15 17:07:40 -0300

    first commit

--- transferenciaBancaria/Program.cs
+++ transferenciaBancaria/Program.cs
@@ -1,28 +1,30 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Transferencia_Bancaria_C_
 {
     class Program
     {
+        static List<Conta> listContas = new List<Conta>();
         static void Main(string[] args)
         {
             string opcaoUsuario = ObterOpcaoUsuario();
             while (opcaoUsuario.ToUpper() != "X"){
                 switch(opcaoUsuario){
                     case "1":
-                        // ListarContas();
+                         ListarContas();
                         break;
                     case "2":
-                        // InserirConta();
+                         InserirConta();
                         break;
                     case "3":
-                        // Transferir();
+                        Transferir();
                         break;
                     case "4":
-                        // Sacar();
+                        Sacar();
                         break;
                     case "5":
-                        // Depositar();
+                        Depositar();
                         break;
                     case "C":
                         Console.Clear();
@@ -37,7 +39,78 @@ namespace Transferencia_Bancaria_C_
             Console.ReadLine();
         }
 
-        
+        private static void Depositar()
+        {
+           Console.Write("Digite o numero da conta: ");
+           int indiceConta = int.Parse(Console.ReadLine());
+
+           Console.Write("Digite o valor a ser depositado: ");
+           double valorDepositao = double.Parse(Console.ReadLine());
+
+           listContas[indiceConta].Depositar(valorDepositao);
+            
+        }
+
+        private static void Transferir()
+        {
+            Console.Write("Digite o numero da conta de origem: ");
+            int indiceContaOrigem = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o numero da conta de destino: ");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser transferido: ");
+            double valortransferido = double.Parse(Console.ReadLine());
+
+            listContas[indiceContaOrigem].Transferir(valortransferido, listContas[indiceContaDestino]);
+            
+        }
+
+        private static void Sacar()
+        {
+            Console.Write("Digite o numero da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor a ser sacado: ");
+            double valorDepositado = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorDepositado);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas: ");
+           if( listContas.Count == 0){
+               Console.WriteLine("Nenhum conta cadastrada.");
+               return;
+           }
+           for (int i = 0; i < listContas.Count; i++){
+               Conta conta = listContas[i];
+               Console.Write("#{0} -", i);
+               Console.WriteLine(conta);
+           }
+        }
+
+        private static void InserirConta()
+        {
+           Console.WriteLine("Inserir nova Conta: ");
+           Console.Write("Digite 1 para conta Fisica ou 2 para juridica: ");
+           int entradaTipoConta = int.Parse(Console.ReadLine());
+
+           Console.Write("Digite o Nome do Cliente: ");
+           string entradaNome = Console.ReadLine(); 
+
+           Console.Write("Digite o Saldo: ");
+           double entradaSaldo = double.Parse(Console.ReadLine());
+
+           Console.Write("Digite o credito: ");
+           double entradaCredito = double.Parse(Console.ReadLine());
+
+           Conta novaConta = new Conta (tipoConta: (TipoConta)entradaTipoConta,
+           saldo: entradaSaldo, credito: entradaCredito, nome: entradaNome);
+
+           listContas.Add(novaConta);
+        }
 
         private static string ObterOpcaoUsuario(){
             Console.WriteLine();
